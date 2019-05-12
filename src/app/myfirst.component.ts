b8100ad37d225ae8b207528f7dffa98fc44b247a
PedroRib76 <p.alessandro@uol.com.br>
2019-05-12 13:48:26 -0300

    First class about Angular Learning

---
+++ src/app/myfirst.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    //tag HTML to call component
+    selector: 'myfirst',
+    templateUrl: './myfirst.component.html'
+})
+//When we need a class ou interface to be used outside the current file, use the EXPORT in front
+export class MyFirstComponent{
+
+}
